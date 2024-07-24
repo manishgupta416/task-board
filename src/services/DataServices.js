041089497ced716f2376b705a3413290216322a8
Manish <manish.info2020@gmail.com>
2024-07-24 14:34:27 +0530

    fix data issue

--- src/services/DataServices.js
+++ src/services/DataServices.js
@@ -2,15 +2,16 @@ import axios from "axios";
 import { data } from "../data/data";
 
 export const getAllTask = async (dataDispatch) => {
-  try {
-    const response = await axios.get("https://gcp-mock.apiwiz.io/v1/tasks", {
-      headers: "x-tenant: b4349714-47c7-4605-a81c-df509fc7e653",
-    });
-    if (response.status === 200) {
-      console.log(response.data);
-      dataDispatch({ type: "getAllTask", payload: data });
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   const response = await axios.get("https://gcp-mock.apiwiz.io/v1/tasks", {
+  //     headers: "x-tenant: b4349714-47c7-4605-a81c-df509fc7e653",
+  //   });
+  //   if (response.status === 200) {
+  //     console.log(response.data);
+  //     dataDispatch({ type: "getAllTask", payload: data });
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  return dataDispatch({ type: "getAllTask", payload: data });
 };
